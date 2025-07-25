20ee9b3df3bb757e9b0e3dfa1a2a2988790a0c0f
user.name : "Yagyesh <user.email : "yagyeshyadav2005@gmail.com>
2025-07-25 20:23:16 +0530

    solved 217 using sorting

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,16 +1,29 @@
 //217(leetcode )
 
+import java.util.Arrays;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        for(int i =0 ; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
+        Arrays.sort(nums);
+            //nums.length-1 gives the index of the last element
+        for(int i = 0 ; i <nums.length-1; i++){
+            // checking for duplicates with in arrray
+            if(nums[i] == nums[i + 1]){
+                return true;
             }
         }
-        return false;
-    }
+          return false;
     
+    }
+  
+}
+
+public class ContainsDuplicate {
+    //example
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1,2,3,1};
+
+        System.out.println(solution.containsDuplicate(nums1));
+    }
 }
